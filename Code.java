e8064004ae0866651bef0a52b4759817641a599b
Rajeev <rajeev21c@outlook.com>
2024-06-19 09:44:39 +0530

    Updated code

--- Code.java
+++ Code.java
@@ -2,8 +2,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Collections;
-import java.util.PriorityQueue;
 import java.io.IOException;
 
 public class Code {
